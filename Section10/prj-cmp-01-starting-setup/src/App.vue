eb1ae5c57e58028ab4202190213a5c1900707b6b
chan-gon <ruggedcafe@gmail.com>
2023-12-21 23:39:14 +0900

    섹션10/진행중

---
+++ Section10/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          Link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          Link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
