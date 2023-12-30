5da39bed5848a8f99b39dcd2c9ef329a38b267bb
chan-gon <ruggedcafe@gmail.com>
2023-12-30 22:39:19 +0900

    섹션15/Vuex

---
+++ Section15/vuex-11-a-challenge-starting-code/src/pages/ProductsList.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <ul>
+      <product-item
+        v-for="prod in products"
+        :key="prod.id"
+        :id="prod.id"
+        :title="prod.title"
+        :image="prod.image"
+        :description="prod.description"
+        :price="prod.price"
+      ></product-item>
+    </ul>
+  </section>
+</template>
+
+<script>
+import ProductItem from '../components/products/ProductItem.vue';
+
+export default {
+  components: {
+    ProductItem,
+  },
+  computed: {
+    products() {
+      // products.js는 namespace:true 이기 때문에 index.js에 정의된 네임스페이스 키 값을 사용해서 호출
+      return this.$store.getters['prods/products'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 40rem;
+}
+</style>
