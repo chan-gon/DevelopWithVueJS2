a63266a44be05475b7a01d3bfe31a6ece1c2b431
chan-gon <ruggedcafe@gmail.com>
2024-01-05 23:51:12 +0900

    섹션16/256강

--- Section16(MainProject)/main-prj-01-starting-setup/src/pages/requests/RequestsReceive.vue
+++ Section16(MainProject)/main-prj-01-starting-setup/src/pages/requests/RequestsReceive.vue
@@ -1,10 +1,17 @@
 <template>
+  <!-- 
+    !! 마크는 참인 값을 실제 불리언으로 변환해준다
+   -->
+  <base-dialog :show="!!error" title="An error occur" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests && !isLoading">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
@@ -24,6 +31,12 @@ export default {
   components: {
     RequestItem,
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
@@ -31,6 +44,24 @@ export default {
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
     },
+  },
+  // RequestsReceive.vue 컴포넌트가 생성될 때 실행되는 메서드 지정
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed!';
+      }
+      this.isLoading = false;
+    },
+  },
+  handleError() {
+    this.error = null;
   },
 };
 </script>
--- Section16(MainProject)/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ Section16(MainProject)/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -45,9 +45,9 @@ export default {
       const coach = {
         id: key,
         firstName: responseData[key].firstName,
-        lastName: responseData[key].last,
+        lastName: responseData[key].lastName,
         description: responseData[key].description,
-        hourlyRate: responseData[key].rate,
+        hourlyRate: responseData[key].hourlyRate,
         areas: responseData[key].areas,
       };
       coaches.push(coach);
